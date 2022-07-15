3b98d2a0ada1e4ee14a06ffc6bc4aca892ac2d97
Elineora <109291643+Elineora07@users.noreply.github.com>
2022-07-15 23:04:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let weather = {
+    apiKey: "968f323d53d10e35332efec57aab7cd3",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+             + city 
+             + "&units=metric&appid=" 
+             + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data))
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed)
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = 
+        "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value)
+    }
+};
+
+document.querySelector(".search button").addEventListener("click", 
+    function () {
+        weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function(e) {
+    if (e.key == 'Enter') {
+        weather.search();
+    }
+});
